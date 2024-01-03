e2af38caab6f14ad4318325b8e6cbd3046621af0
popelolole <pellebe@kth.se>
2024-01-03 20:29:13 +0100

    add test

--- src/main/java/se/kthraven/journalservice/JournalServiceApplication.java
+++ src/main/java/se/kthraven/journalservice/JournalServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JournalServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("TEST");
         SpringApplication.run(JournalServiceApplication.class, args);
     }
 
