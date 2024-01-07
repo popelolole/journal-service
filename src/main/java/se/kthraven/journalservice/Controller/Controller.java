ca002f8aa30da7171b59a0da335ca9096bcd420c
popelolole <pellebe@kth.se>
2024-01-07 18:33:29 +0100

    add keycloak config

--- src/main/java/se/kthraven/journalservice/Controller/Controller.java
+++ src/main/java/se/kthraven/journalservice/Controller/Controller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import se.kthraven.journalservice.Model.IJournalService;
 import se.kthraven.journalservice.Model.classes.Doctor;
@@ -25,14 +26,12 @@ public class Controller {
         return patient;
     }
 
-    @PreAuthorize("hasRole('ROLE_DOCTOR') or hasRole('ROLE_OTHER')")
     @PostMapping("/patient")
     public ResponseEntity<String> createPatient(@RequestBody Patient patient){
         journalService.createPatient(patient);
         return new ResponseEntity<>("Person created successfully", HttpStatus.CREATED);
     }
 
-    @PreAuthorize("hasRole('ROLE_DOCTOR') or hasRole('ROLE_OTHER')")
     @GetMapping("/doctor")
     public Doctor getDoctor(@RequestParam(value = "id") String id){
         Doctor doctor = journalService.getDoctor(id);
@@ -51,14 +50,12 @@ public class Controller {
         return encounter;
     }
 
-    @PreAuthorize("hasRole('ROLE_DOCTOR') or hasRole('ROLE_OTHER')")
     @PostMapping("/encounter")
     public ResponseEntity<String> createEncounter(@RequestBody Encounter encounter){
         journalService.createEncounter(encounter);
         return new ResponseEntity<>("Encounter created successfully", HttpStatus.CREATED);
     }
 
-    @PreAuthorize("hasRole('ROLE_DOCTOR') or hasRole('ROLE_OTHER')")
     @PostMapping("/encounter/observation")
     public Observation createObservation(@RequestBody Observation observation, @RequestParam(value = "encounterId") String encounterId){
         journalService.createObservation(observation, encounterId);
--- src/main/java/se/kthraven/journalservice/config/SecurityConfig.java
+++ src/main/java/se/kthraven/journalservice/config/SecurityConfig.java
@@ -2,13 +2,23 @@ package se.kthraven.journalservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -17,18 +27,8 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .authorizeHttpRequests(auth -> auth
-                        //.requestMatchers("/login").permitAll()
-                        .anyRequest().authenticated())
-                .httpBasic(basic -> basic.init(http))
-                .csrf(csrf -> csrf.disable())
-                .addFilterBefore(new SimpleAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .build();
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()).csrf(csrf -> csrf.disable());
+        http.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
     }
-
-    /*@Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }*/
 }
